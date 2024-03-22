5cc537878d73cc92dcbe4c3e7f4be8fb636e1afd
Jarib Cockerel <jarib.cockerel@outlook.com>
2024-03-21 19:11:26 -0500

    Adding updates and following along with Jasmine test examples in taxes and taxes.test

---
+++ setUp_ex1/taxes.js
@@ -0,0 +1,53 @@
+function calculateTaxes(income) {
+  if (!Number.isFinite(income)) {
+    throw new Error("BAD INCOME");
+  }
+  if (income > 30000) {
+    return income * 0.25;
+  } else {
+    return income * 0.15;
+  }
+}
+// best to seperate test from actual application code
+// you could write jasmine but not best practice
+// same file name with file.test.js for script
+
+//format is expect(someValue).someMatcher(...)
+
+//remove dupliaces of values
+
+function removeDupes(values) {
+  return [...new Set(values)]; //return value as an array to remove duplicates
+}
+
+function removeCopyCat(value) {
+  const arr = [...new Set(value)];
+  if (typeof value === "string") return arr.join(""); //return value that remove duplicate strings 'hello' to 'helo' as the l was removed since it was a duplicate
+  //the "" will return the value into a string
+  return;
+}
+
+//tobeInstanceOf
+
+//using.not.matcher
+function remove(arr, val) {
+  return arr.filter((el) => {
+    return el !== val;
+  });
+} //return value outside of array so [1-6], 6 which test should return 6
+
+// function enterNum(numEnter) {
+//   if (!Number.isFinite(numEnter)) {
+//     throw new Error("Invalid Number input!!");
+//   } else {
+//     return numEnter;
+//   }
+// }
+
+const userNames = []; //passthru result in array form
+
+let nameInput = document.getElementById("userName"); //select input from userName submission
+
+function subForm() {
+  userNames.push(nameInput.value);
+} //push value of userNames into an array format
---
+++ setUp_ex1/taxes.test.js
@@ -0,0 +1,68 @@
+// need to load after application in the index.html
+//test taxes for high income brackets
+describe("calculate high tax bracket", function () {
+  it("should calculate the high tax bracket", function () {
+    // expect(someValue).someMatcher(...)
+    //toEqual is a matcher and there are many more of them
+    expect(calculateTaxes(50000)).toEqual(12500);
+    //expect 50000 * 0.25 to equate to 12500
+    expect(calculateTaxes(100000)).toEqual(25000);
+    //expect 100000 * 0.25 to equate to 25000
+  });
+});
+
+describe("Calculate low tax bracket", function () {
+  it("should calculate the high tax bracket", function () {
+    // expect(someValue).someMatcher(...)
+    //toEqual is a matcher and there are many more of them
+    expect(calculateTaxes(50000)).toEqual(12500);
+    expect(calculateTaxes(100000)).toEqual(25000);
+
+    //   expect("hello").toEqual("good-bye");
+    //failure shows which it(first argument) went wrong + expected hello to equal goodbye ALso shoes red 1 failure
+  });
+});
+
+it("should calculate the low tax bracket", function () {
+  expect(calculateTaxes(10000)).toEqual(1500);
+  expect(calculateTaxes(1000)).toEqual(150);
+  //   edge case
+  expect(calculateTaxes(0)).toEqual(0);
+});
+
+describe("Remove Duplicates", function () {
+  it("should remove duplicates from an array", function () {
+    //   expect(removeDupes([1, 1, 2, 2, 3, 4])).toBe([1, 2, 3, 4]);
+    //toBe is same as === which requires the references to be the same vs use of matcher toEqual
+    expect(removeDupes([1, 1, 2, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+  }); //expect function to not obtain duplicate array
+
+  it("should remove duplicates from a string", function () {
+    expect(removeCopyCat("hello")).toBe("helo");
+    expect(removeCopyCat("hello")).toBeInstanceOf(String); //expect the actual to be an instance of the expected class
+    //   expect(removeCopyCat("hello")).toBeInstanceOf(Number); //expect the actual to be an instance of the expected number which triggered a failure
+  }); //expect functionto not obtain duplicate string
+
+  it("should remove value from array", function () {
+    expect(remove([1, 2, 3, 4, 5, 6], 6)).not.toContain(6);
+  }); //expect the return function to not obtain 6
+});
+
+// it("Should reject input if no value input", function () {
+//   expect(() => enterNum("Hi").toThrowError());
+//   expect(() => enterNum().toThrowError());
+//   expect(() => enterNum(true).toThrowError());
+// });
+
+//test function
+// describe a group of related test in the same namespace
+describe("subform() test", () => {
+  it("should input val to usernames array", () => {
+    subForm();
+    // expect(userName.length).toBe(1); //failure but will show in Jasmine js:59:29 the error
+    expect(userNames.length).toBe(1);
+  });
+  it("saves long usernames", () => {
+    nameInput;
+  });
+});
